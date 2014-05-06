8ed590566e24fb33b75cf9816a46de19a8649b9d
codeswholesale <devteam@codeswholesale.com>
2014-05-06 19:05:43 +0200

    Added stock's quantity information to product resource

---
+++ src/CodesWholesale/Util/OAuthError.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CodesWholesale\Util;
+
+class OAuthError extends \RuntimeException
+{
+    public function __construct($message = "") {
+        parent::__construct($message);
+    }
+}
