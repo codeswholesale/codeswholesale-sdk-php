d8ddeb7c47e0054ee353a235b25cddb8fb8ba413
AW <myperit@gmail.com>
2014-12-02 13:39:04 +0100

    Add new fields

--- src/CodesWholesale/Resource/Code.php
+++ src/CodesWholesale/Resource/Code.php
@@ -2,38 +2,49 @@
 
 namespace CodesWholesale\Resource;
 
-
+use CodesWholesale\Client;
 use CodesWholesale\CodesWholesale;
 
-class Code extends Resource {
+class Code extends Resource
+{
 
-    const CODE_TYPE_PROP_NAME       = "codeType";
-    const CODE_PROP_NAME            = "code";
-    const FILE_NAME_PROP_NAME       = "fileName";
+    const CODE_TYPE_PROP_NAME = "codeType";
+    const CODE_PROP_NAME = "code";
+    const FILE_NAME_PROP_NAME = "fileName";
 
-    const IMAGE                     = "CODE_IMAGE";
-    const TEXT                      = "CODE_TEXT";
-    const PREORDER                  = "CODE_PREORDER";
+    const IMAGE = "CODE_IMAGE";
+    const TEXT = "CODE_TEXT";
+    const PREORDER = "CODE_PREORDER";
 
-    public function isPreOrder() {
+    public static function get($href, array $options = array())
+    {
+        return Client::get($href, CodesWholesale::CODE, null, $options);
+    }
+
+    public function isPreOrder()
+    {
         return $this->getProperty(self::CODE_TYPE_PROP_NAME) == self::PREORDER;
     }
 
-    public function isImage() {
+    public function isImage()
+    {
         return $this->getProperty(self::CODE_TYPE_PROP_NAME) == self::IMAGE;
     }
 
-    public function isText() {
+    public function isText()
+    {
         return $this->getProperty(self::CODE_TYPE_PROP_NAME) == self::TEXT;
     }
 
-    public function getFileName() {
+    public function getFileName()
+    {
         return $this->getProperty(self::FILE_NAME_PROP_NAME);
     }
 
-    public function getCode() {
+    public function getCode()
+    {
         $code = trim($this->getProperty(self::CODE_PROP_NAME));
-        if(empty($code) || strlen($code) == 0 ){
+        if (empty($code) || strlen($code) == 0) {
             $resource = $this->dataStore->getResource($this->getHref(), CodesWholesale::CODE, array());
             $this->setProperties($resource->getProperties());
         }
--- src/CodesWholesale/Resource/CodeList.php
+++ src/CodesWholesale/Resource/CodeList.php
@@ -6,7 +6,13 @@ use CodesWholesale\CodesWholesale;
 
 class CodeList extends AbstractCollectionResource {
 
-    function getItemClassName() {
+    const ORDER_ID = "orderId";
+
+    public function getOrderId() {
+        return $this->getProperty(self::ORDER_ID);
+    }
+
+    public function getItemClassName() {
         return CodesWholesale::CODE;
     }
 }
